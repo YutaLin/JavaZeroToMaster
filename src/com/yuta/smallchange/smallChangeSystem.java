db7537480ae0b5a4ed3d7de4bc3371aabf9aaa6b
yuta <linbolin1230@gmail.com>
2024-11-16 12:52:13 -0500

    implemented income functionality

--- src/com/yuta/smallchange/smallChangeSystem.java
+++ src/com/yuta/smallchange/smallChangeSystem.java
@@ -1,5 +1,7 @@
 package com.yuta.smallchange;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class smallChangeSystem {
@@ -7,14 +9,26 @@ public class smallChangeSystem {
     // complete selection menu and make user select
 
     public static void main(String[] args) {
+
+        // 1.define select menu
         boolean loop = true;
         Scanner sc = new Scanner(System.in);
         String key = "";
+
+        // 2. change detail
+        String details = "------change detail-----";
+
+        // 3. change income
+        double money = 0;
+        double balance = 0;
+        Date date = null;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
         do {
             System.out.println("=====small change menu=====");
             System.out.println("\t1. change detail");
             System.out.println("\t2. change income");
-            System.out.println("\t3. outcome");
+            System.out.println("\t3. change outcome");
             System.out.println("\t4. exit");
 
             System.out.print("Please enter your choice(1-4): ");
@@ -22,10 +36,18 @@ public class smallChangeSystem {
 
             switch (key) {
                 case "1":
-                    System.out.println("1. change detail");
+                    System.out.println(details);
                     break;
                 case "2":
-                    System.out.println("2. change income");
+                    System.out.print("Income amount: ");
+                    money = sc.nextDouble();
+                    // validate money range( > 0)
+
+                    balance += money;
+                    // concatenate balance message
+                    date = new Date(); // get current date time
+                    details += "\nIncome: +\t" + money + "\t" + sdf.format(date) + "\t" + balance;
+                    break;
                 case "3":
                     System.out.println("3. outcome");
                     break;
